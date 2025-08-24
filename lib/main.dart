df0822de6da00d57c4cad5348f7275b5bf50e1e7
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-08-24 21:49:12 +0300

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,12 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white.withValues(alpha: 0.2),
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins',
+        // textTheme: ,
+      ),
       home: const NotesView(),
     );
   }
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -9,7 +10,70 @@ class NotesViewBody extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(children: [CustomAppBar()]),
+        child: Column(children: [CustomAppBar(), NoteItem()]),
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 24),
+      decoration: BoxDecoration(
+        color: const Color(0xfffffcCbc),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            contentPadding: EdgeInsets.only(right: 0),
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(color: Colors.black, fontSize: 27),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 18.0, bottom: 5),
+              child: Text(
+                'Build your career with tharwat samy',
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 122, 122, 122),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 30.0,
+                  left: 16,
+                  right: 10,
+                ),
+                child: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 23,
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(
+              'May21 , 2022',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 122, 122, 122),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
